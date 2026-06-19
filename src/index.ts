55dc7c84d6ace2f7b362d0a267c4066ee88997e0
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-06-19 22:48:15 +0530

    backend setup and database connect

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+const MONGO_URL = process.env.MONGO_URL || ""
+
+// globle middlewares
+app.use(cors());
+app.use(express.json());
+
+// mount routes
+app.get("/", (req, res) => {
+    res.send("Backend Running...");
+});
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("DB connected");
+    })
+    .catch((err) => {
+        console.log("reson : " , err.message);
+        console.error("fail to DB connect");
+    })
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
