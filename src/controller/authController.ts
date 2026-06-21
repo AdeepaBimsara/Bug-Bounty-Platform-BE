85a2b43531bdea5abbbe8198548477c3aa38152f
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-06-21 12:55:48 +0530

    implement login

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express"
 import {UserModel, UserRole} from "../models/userModel"
+import {signAccessToken,signRefreshToken} from "../ultils/tokens"
+import {AuthRequest} from "../middleware/auth"
 import dotenv from "dotenv"
 import bcrypt from "bcryptjs"
-
+import jwt from "jsonwebtoken"
 
 dotenv.config()
+
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string
 
 export const registerUser = async (req: Request, res: Response) => {
     try{
@@ -39,4 +43,90 @@ export const registerUser = async (req: Request, res: Response) => {
         })
 
     }
+}
+
+export const login = async (req: Request, res: Response) => {
+    try{
+        const {email,password} = req.body
+
+        const user = await UserModel.findOne({email})
+
+        console.log("User : " ,user);
+        
+        if(!user){
+            return res.status(401).json({message: "Invalid credentials"})
+        }
+
+        const isValid = await bcrypt.compare(password, user?.password)
+
+        if(!isValid){
+           return res.status(401).json({message: "Invalid credintials"})
+        }
+
+        console.log("password mach: " , isValid);
+        
+
+        const accessToken = signAccessToken(user)
+        const refreshToken = signRefreshToken(user)
+
+         res.status(200).json({
+            message: "success",
+            data: {
+                email: user?.email,
+                roles: user?.role,
+                accessToken,
+                refreshToken
+            }
+    })
+
+    }catch(err){
+
+      console.error(err);
+
+      res.status(500).json({
+        message: "Login fail",
+        error: err
+      })
+      
+
+    }
+}
+
+export const getMyDetails = async (req: AuthRequest, res: Response) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" })
+  }
+  const user = await UserModel.findById(req.user.sub).select("-password")
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found"
+    })
+  }
+
+  const { email, role, _id } = user
+
+  res.status(200).json({ message: "ok", data: { id: _id, email, role } })
+}
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const { refreshToken } = req.body
+  try {
+    if (!refreshToken) {
+      return res.status(400).json({ message: "Token required..!" })
+    }
+    const payload = jwt.verify(refreshToken, JWT_REFRESH_SECRET)
+    const userId = payload.sub
+    const user = await UserModel.findById(userId)
+    if (!user) {
+      return res.status(403).json({ message: "Invalid or expire token" })
+    }
+    const newAccessToken = signAccessToken(user)
+    res.status(200).json({
+      message: "",
+      data: { accessToken: newAccessToken }
+    })
+  } catch (err) {
+    res.status(403).json({ message: "Invalid or expire token" })
+  }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import { registerUser } from "../controller/authController";
+import { getMyDetails, login, registerUser,refreshToken } from "../controller/authController";
+import { authenticate } from "../middleware/auth";
+import { requireRole } from "../middleware/roles";
+import { UserRole } from "../models/userModel";
 
 const router = Router()
 
 router.post("/register",registerUser)
+
+router.post("/login", login)
+
+router.get("/me", authenticate, getMyDetails)
+
+// router.post(
+//   "/admin/register",
+//   authenticate,
+//   requireRole([UserRole.ADMIN]),
+//   registerAdmin
+// )
+
+router.post("/refresh", refreshToken)
+
+
 
 export default router
