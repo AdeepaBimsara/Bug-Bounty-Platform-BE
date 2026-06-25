4d1f55bfc7eb08521df941e65390ca17df0d8b3b
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-06-25 12:16:35 +0530

    implement programs get,update and delete

--- src/controller/programController.ts
+++ src/controller/programController.ts
@@ -39,3 +39,98 @@ export const createProgram = async (req: AuthRequest, res: Response) => {
     });
   }
 };
+
+export const getMyPrograms = async (
+  req: AuthRequest,
+  res: Response
+) => {
+  try {
+    const companyId = req.user.sub;
+
+    const programs = await ProgramModel.find({
+      companyId,
+    });
+
+    res.status(200).json({
+      data: programs,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to fetch programs",
+    });
+  }
+};
+
+export const deleteProgram = async (
+  req: AuthRequest,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const companyId = req.user.sub;
+
+    const program = await ProgramModel.findOneAndDelete({
+      _id: id,
+      companyId,
+    });
+
+    if (!program) {
+      return res.status(404).json({
+        message: "Program not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Program deleted successfully",
+    });
+
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Delete failed",
+    });
+  }
+};
+
+export const updateProgram = async (
+  req: AuthRequest,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const companyId = req.user.sub;
+
+    const updatedProgram =
+      await ProgramModel.findOneAndUpdate(
+        {
+          _id: id,
+          companyId,
+        },
+        req.body,
+        {
+          new: true,
+        }
+      );
+
+    if (!updatedProgram) {
+      return res.status(404).json({
+        message: "Program not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Program updated successfully",
+      data: updatedProgram,
+    });
+
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      message: "Update failed",
+    });
+  }
+};
--- src/routes/programRoutes.ts
+++ src/routes/programRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth";
-import { createProgram } from "../controller/programController";
+import { createProgram, deleteProgram, getMyPrograms, updateProgram } from "../controller/programController";
 
 const router = Router();
 
 router.post("/", authenticate, createProgram);
+
+router.get("/my-programs", authenticate,getMyPrograms);
+
+router.delete("/:id", authenticate,deleteProgram);
+
+router.put("/:id",authenticate,updateProgram);
 
 export default router;
